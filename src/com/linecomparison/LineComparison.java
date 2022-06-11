2d8ae500d06259d78d2e54bd18193fa6f75d244d
srajashekhar <udayarajashekhar@gmail.com>
2022-06-11 16:36:47 +0530

    Added:Calculating the length of the Two line

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -1,9 +1,25 @@
 package com.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program...");
-	}
 
+		int x1;
+		int y1;
+		int x2;
+		int y2;
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Four numbers:- ");
+		x1 = scanner.nextInt();
+		y1 = scanner.nextInt();
+		x2 = scanner.nextInt();
+		y2 = scanner.nextInt();
+
+		double Length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("Length of Line is: " + Length);
+	}
 }
